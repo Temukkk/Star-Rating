d5c26db702828314fafc2f609a502181d2c6d04e
Temukkk <temuulengansuh1074@gmail.com>
2024-10-03 22:27:29 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Select all star elements
+const stars = document.querySelectorAll(".stars i");
+
+stars.forEach((star, index1) => {
+    star.addEventListener("click", () => {
+        stars.forEach((star, index2) => {
+            if (index1 >= index2) {
+                star.classList.add("active");
+            } else {
+                star.classList.remove("active");
+            }
+        });
+    });
+});
